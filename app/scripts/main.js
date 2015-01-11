c83e34eeabe7c2ee628a54fb93b8f8ec0edd6ae6
Tim Whitacre <tim@timw.co>
2015-01-10 22:00:21 -0500

    Copy over files and set up structure.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,48 @@
-console.log('The Iron Yard Rocks');
+$(document).ready(function(){
+
+  //------------------------------------//
+  //Navbar//
+  //------------------------------------//
+    	var menu = $('.navbar');
+    	$(window).bind('scroll', function(e){
+    		if($(window).scrollTop() > 140){
+    			if(!menu.hasClass('open')){
+    				menu.addClass('open');
+    			}
+    		}else{
+    			if(menu.hasClass('open')){
+    				menu.removeClass('open');
+    			}
+    		}
+    	});
+
+
+  //------------------------------------//
+  //Scroll To//
+  //------------------------------------//
+  $(".scroll").click(function(event){
+  	event.preventDefault();
+  	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+
+  });
+
+
+  //------------------------------------//
+  //Scroll To Top//
+  //------------------------------------//
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 300) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
+});
+
